0a71083f068ce680852437ab69ed523443449f4c
jihwan98 <jihwan98.dev@gmail.com>
2023-07-21 21:32:14 +0900

    회원 서비스 코드 & 테스트 코드

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,43 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*
+    회원가입
+    */
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // 같은 이름이 있는 중복 회원 X
+        // Null 값이 있을 수 있는 경우라면 Optional로 감싸서 많이 사용함
+        // result.get();으로 꺼내서 사용해도 되지만 권장되지 않음
+        memberRepository.findByName(member.getName())
+                .ifPresent(member1 -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findall();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findByID(memberId);
+    }
+}

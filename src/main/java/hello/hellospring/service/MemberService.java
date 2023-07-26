582f5720311d34202b9a791b0ec90dfeda132ac8
jihwan98 <jihwan98.dev@gmail.com>
2023-07-26 15:32:00 +0900

    AOP

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -35,6 +35,9 @@ public class MemberService {
                 });
     }
 
+    /*
+    * 전체 회원 조회
+    * */
     public List<Member> findMembers() {
         return memberRepository.findAll();
     }
